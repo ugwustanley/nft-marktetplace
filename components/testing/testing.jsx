651892d4f9f22b1dacf4d136b3e5669dc7f1436d
ugwustanley <ugwuchiagoziestanley@gmail.com>
2022-02-20 16:51:08 +0100

    edit on variables, test page implemented

---
+++ components/testing/testing.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import styles from './testing.module.scss'
+
+const Testing = () => {
+  return (
+    <div className={styles.container}>
+      <h1>H1 Hello</h1>
+      <h2>H2 Hello</h2>
+      <h3>H3 hello</h3>
+      <h4>H4 hello</h4>
+      <h5>H5 hello</h5>
+      <h6>H6 Hello</h6>
+    </div>
+  );
+};
+
+export default Testing;
---
+++ pages/test.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Testing from '@/components/testing/testing.jsx';
+
+const Test = ( ) =>{
+    return(
+      <div>
+           <Testing />
+      </div>
+    )
+}
+
+export default Test

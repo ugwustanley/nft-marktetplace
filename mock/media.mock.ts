c4e8a935e47d741659e938fb74e10b48461c8335
ugwustanley <ugwuchiagoziestanley@gmail.com>
2022-02-21 15:47:05 +0100

    change in nav iteration three

--- mock/media.mock.ts
+++ mock/media.mock.ts
@@ -5,14 +5,14 @@ interface Iuser{
 
 
 const user: Iuser = {
-    profile: 'https://res.cloudinary.com/dqzhuopqp/image/upload/t_media_lib_thumb/v1645365081/samples/jpg_foewhj_afpxrr.jpg',
+    profile: 'https://res.cloudinary.com/dqzhuopqp/image/upload/v1645365081/samples/jpg_foewhj_afpxrr.jpg',
     signature: 'https://res.cloudinary.com/dqzhuopqp/image/upload/v1645365081/samples/Screenshot_2022-02-06_at_16.08.52_izungm_wmho2r.jpg',
 }
 
 const nfts = {
-    sampleOne: 'https://res.cloudinary.com/dqzhuopqp/image/upload/v1645365083/samples/JuliaMain_scelkx.webp',
+    sampleOne: 'https://res.cloudinary.com/dqzhuopqp/image/upload/v1645365083/samples/JuliaMain_scelkx.jpg',
     sampleTwo: 'https://res.cloudinary.com/dqzhuopqp/image/upload/v1645365082/samples/nft-1_aihzzu.jpg',
-    sampleThree: 'https://res.cloudinary.com/dqzhuopqp/image/upload/t_media_lib_thumb/v1645365081/samples/Jim_placeholder_erelhf.jpg',
+    sampleThree: 'https://res.cloudinary.com/dqzhuopqp/image/upload/v1645365081/samples/Jim_placeholder_erelhf.jpg',
     sampleFour: 'https://res.cloudinary.com/dqzhuopqp/image/upload/v1645436520/samples/s-4_lqpy8e.jpg',
 }
 

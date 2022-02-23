10c292a98234f808cf1c3b63623790178ab4d1ec
ugwustanley <ugwuchiagoziestanley@gmail.com>
2022-02-23 14:00:05 +0100

    issue on svg fixed

--- components/exploreLink/exploreLink.tsx
+++ components/exploreLink/exploreLink.tsx
@@ -17,7 +17,7 @@ const ExploreLink = ({bg, height, text}:props): ReactElement => {
       }}>
         <div className={styles.explorelink__info}>
                <h2>{text}</h2>
-               <svg xmlns="http://www.w3.org/2000/svg" width="192" height="192" fill="#000000" viewBox="0 0 256 256"><rect width="256" height="256" fill="none"></rect><line x1="40" y1="128" x2="216" y2="128" fill="none" stroke="#fff" stroke-linecap="round" stroke-linejoin="round" stroke-width="16"></line><polyline points="144 56 216 128 144 200" fill="none" stroke="#fff" stroke-linecap="round" stroke-linejoin="round" stroke-width="16"></polyline></svg>
+               <svg xmlns="http://www.w3.org/2000/svg" width="192" height="192" fill="#000000" viewBox="0 0 256 256"><rect width="256" height="256" fill="none"></rect><line x1="40" y1="128" x2="216" y2="128" fill="none" stroke="#fff" strokeLinecap="round" strokeLinejoin="round" strokeWidth="16"></line><polyline points="144 56 216 128 144 200" fill="none" stroke="#fff" strokeLinecap="round" strokeLinejoin="round" strokeWidth="16"></polyline></svg>
         </div>
     </div>
   )
